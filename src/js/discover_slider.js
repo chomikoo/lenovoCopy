2e193ca27e7150641ad6d70339cfe5300692cfd3
chomikoo <trzepla.szymon@gmail.com>
2017-07-30 19:11:22 +0200

    Update discover_slider.js

--- src/js/discover_slider.js
+++ src/js/discover_slider.js
@@ -36,8 +36,8 @@ $(document).ready(function () {
         
         if (icons[index - 1].src.indexOf("active") == -1) {
                var path = icons[index - 1].src.split(".");
-                icons[index - 1].src = "./img/icon-" + (index + 1) + "active.png";
-                console.log(icons[index - 1].src);
+                icons[index - 1].src = "./img/icon-" + index + "active.png";
+                console.log(icons[index].src);
             }
     }
 });
