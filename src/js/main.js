870e1f6328cb93cef8b0b7d24b83a089ddeb53af
Chomikoo <trzepla.szymon@gmail.com>
2017-08-03 23:46:04 +0200

    fix rwd

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,48 @@
 //Discover more 
 
-$(window).scroll(function () {
-    if ($(this).width() < 949) {
-        var mySiema = new Siema();
-        $(".prev").click(function () {
-            mySiema.prev()
-        });
-        $(".next").click(function () {
-            mySiema.next()
-        });
-    }
-});
+var mySwiper = null;
+	var mySwiperOptions = {
+    	direction: 'horizontal',
+    	loop: true,
+		speed: 500,
+		pagination: '.swiper-pagination',
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev'
+    };
+	var ViewPortWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if( ViewPortWidth < 990){
+		mySwiper = new Swiper ('.swiper-container', mySwiperOptions)
+	};
+	$( window ).resize(function() {
+		var ViewPortWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		if( ViewPortWidth < 990){
+			if( mySwiper == null){
+				mySwiper = new Swiper ('.swiper-container', mySwiperOptions);
+			}
+		}else {
+			if( mySwiper != null){
+				mySwiper.destroy(true, true);
+				mySwiper = null;
+			}
+		}
+	});
+
+// $(window).scroll(function () {
+//     if ($(this).width() < 980) {
+//         var mySiema = new Siema({
+//             loop: true
+//         });
+//         $(".prev").click(function () {
+//             mySiema.prev()
+//         });
+//         $(".next").click(function () {
+//             mySiema.next()
+//         });
+//     }
+//     if($(this).width() >= 980) {
+//         location.reload(true);
+//     }
+// });
 
 //scrollTo
 
