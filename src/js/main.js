19df6f76f1110ffb5edb9aa7d92819b9a15a7fb8
Szymon <trzepla.szymon@gmail.com>
2017-08-04 11:37:21 +0200

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,39 @@
 //Discover more 
 $(document).ready(function() {
+	var mySwiper = null;
+	var mySwiperOptions = {
+		direction: 'horizontal',
+		loop: true,
+		speed: 500,
+		pagination: '.swiper-pagination',
+		nextButton: '.swiper-button-next',
+		prevButton: '.swiper-button-prev'
+	};
+	var ViewPortWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if (ViewPortWidth < 1200) {
+		mySwiper = new Swiper('.swiper-container', mySwiperOptions)
+		console.log("init 1");
+		console.log(mySwiper);
+	};
+	$(window).resize(function() {
+		var ViewPortWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		if (ViewPortWidth < 1200) {
+			if (mySwiper == null) {
+				mySwiper = new Swiper('.swiper-container', mySwiperOptions);
+				console.log(mySwiper);
+				console.log("init 2");
+			}
+		} else {
+			if (mySwiper != null) {
+				mySwiper.destroy(true, true);
+				mySwiper = null;
+			}
+		}
+	});
 
-    var mySwiper = null;
-    var mySwiperOptions = {
-        direction: 'horizontal',
-        loop: true,
-        speed: 500,
-        pagination: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev'
-    };
-    var ViewPortWidth = Math.max(window.innerWidth || 0);
-    if (ViewPortWidth < 990) {
-        console.log(document.documentElement.clientWidth);
-        mySwiper = new Swiper('.swiper-container', mySwiperOptions)
-    };
-    $(window).resize(function() {
-            var ViewPortWidth = Math.max(docwindow.innerWidth || 0);
-            if (ViewPortWidth < 990) {
-                if (mySwiper == null) {
-                    mySwiper = new Swiper('.swiper-container', mySwiperOptions);
-                    console.log(mySwiper);
-                    console.log("Init");
+	//scrollTo
 
-                } else {
-                    if (mySwiper != null) {
-                        mySwiper.destroy(true, true);
-                        mySwiper = null;
-                    }
-                }
-            });
-    });
-
-// $(window).scroll(function () {
-//     if ($(this).width() < 980) {
-//         var mySiema = new Siema({
-//             loop: true
-//         });
-//         $(".prev").click(function () {
-//             mySiema.prev()
-//         });
-//         $(".next").click(function () {
-//             mySiema.next()
-//         });
-//     }
-//     if($(this).width() >= 980) {
-//         location.reload(true);
-//     }
-// });
-
-//scrollTo
-
-$("header a").click(function() {
-    $.scrollTo($(this).attr("href"), 900);
-});
+	$("header a").click(function() {
+		$.scrollTo($(this).attr("href"), 900);
+	});
 });
